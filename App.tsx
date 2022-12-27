f920941085ae7b849aeaaa4193f5513dee6bc646
Zilullah Nazir Hadi <32159429+kaut0@users.noreply.github.com>
2022-12-27 22:03:28 +0800

    StockInventory First Setup

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+const App = (): JSX.Element => {
+  return <SafeAreaProvider />;
+};
+
+export default App;
