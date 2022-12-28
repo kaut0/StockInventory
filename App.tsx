f564e69390be8df26f207de2aaa50e7cf5db51ba
Zilullah Nazir Hadi <32159429+kaut0@users.noreply.github.com>
2022-12-28 21:31:52 +0800

    adding home screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
+import {Home} from 'Screens';
 import React from 'react';
+import {View} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App = (): JSX.Element => {
-  return <SafeAreaProvider />;
+  return (
+    <SafeAreaProvider>
+      <Home />
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,78 @@
+import {Button, Icon} from '@rneui/themed';
+import {Scaling} from 'Styles';
+import * as React from 'react';
+import {View, Text, FlatList, TouchableOpacity} from 'react-native';
+const Home = (): JSX.Element => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'white',
+        flex: 1,
+      }}>
+      {/* ini headers */}
+      <View
+        style={{
+          backgroundColor: '#154c79',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          height: Scaling.Height(60),
+          paddingHorizontal: Scaling.Width(20),
+        }}>
+        <Text
+          style={{
+            color: 'white',
+          }}>
+          Home
+        </Text>
+        <TouchableOpacity>
+          <Icon name="sc-telegram" type="evilicon" color="#517fa4" />
+        </TouchableOpacity>
+      </View>
+      {/* end headers */}
+      {/* body start */}
+      <View
+        style={{
+          padding: Scaling.Width(20),
+        }}>
+        <FlatList
+          data={[1, 2, 3, 4, 5]}
+          showsVerticalScrollIndicator={false}
+          renderItem={() => (
+            <View>
+              <Text>Item</Text>
+            </View>
+          )}
+        />
+      </View>
+      {/* body end */}
+      {/* FLoating Button */}
+      <View
+        style={{
+          position: 'absolute',
+          bottom: Scaling.Height(70),
+          right: Scaling.Width(20),
+        }}>
+        <Button
+          onPress={() => {
+            console.log('makan');
+          }}
+          buttonStyle={{
+            height: Scaling.Height(75),
+            width: Scaling.Height(75),
+            borderRadius: Scaling.Height(40),
+            alignItems: 'center',
+          }}
+          icon={{
+            name: 'home',
+            type: 'font-awesome',
+            size: 35,
+            color: 'white',
+          }}
+        />
+      </View>
+      {/* End FLoating Button */}
+    </View>
+  );
+};
+export default Home;
